18c925ea2749a05fab2b0e6510423f7f0d669658
fgryson <fgryson@gmail.com>
2025-08-09 15:53:31 +0100

    added file writing|cleaned file info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,28 +1,24 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    response = ""
-    directory_string = "current" if directory == "." else f"'{directory}'"
-    response += f'Result for {directory_string} directory:\n'
-    full_path = os.path.abspath(os.path.join(working_directory, directory))
-
-
-    if not full_path.startswith(os.path.abspath(working_directory)):
-        response += f'    Error: Cannot list "{directory}" as it is outside the permitted working directory\n'
-        return response
-    if not os.path.isdir(full_path):
-        response += f'    Error: "{directory}" is not a directory\n'
-        return response
-
     try:
-        contents = os.listdir(full_path)
-        for file in contents:
-            file_path = os.path.join(full_path, file)
-            file_size = os.path.getsize(file_path)
-            is_file_dir = os.path.isdir(file_path)
-            response += f' - {file}: file_size={file_size} bytes, is_dir={is_file_dir}\n'
+        full_path = os.path.abspath(os.path.join(working_directory, directory))
 
-        return response
+        if not full_path.startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(full_path):
+            return f'Error: "{directory}" is not a directory'
+
+        return "\n".join(map(file_summary(full_path), os.listdir(full_path)))
     except Exception as err:
-        response += f'    Error: {err}\n'
-        return response
+        return f'Error: {err}'
+
+def file_summary(full_path):
+    def summarizer(file):
+        file_path = os.path.join(full_path, file)
+        file_size = os.path.getsize(file_path)
+        is_file_dir = os.path.isdir(file_path)
+        return f' - {file}: file_size={file_size} bytes, is_dir={is_file_dir}'
+
+    return summarizer
+    
---
+++ functions/write_file.py
@@ -0,0 +1,14 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    try:
+        full_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+        if not full_path.startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        with open(full_path, "w") as f:
+            f.write(content)
+
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as err:
+        return f'Error: {err}'
